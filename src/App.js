0ccd122f9240eb38ccafd540c246ba75f25f56fd
Rodolfo Perez <rodolfobeste@gmail.com>
2019-04-12 17:09:50 +0300

    Created and styled navigation component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './styles/App.css';
 import Header from "./components/header/Header";
+import Nav from "./components/navigation/Nav";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <Nav />
       </div>
     );
   }
---
+++ src/components/navigation/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./nav.css";
+
+const Nav = () => {
+    return(
+        <nav>
+            <ul className="mainNav">
+                <li>People´s List</li>
+                <li>Add New Person</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
