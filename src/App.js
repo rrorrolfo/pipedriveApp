a4e21939af0a008dba6b4c19f8338be037e3fa5d
Rodolfo Perez <rodolfobeste@gmail.com>
2019-04-12 18:08:19 +0300

    Installed react router and added root route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './styles/App.css';
+import { 
+  BrowserRouter,
+  Route  } from "react-router-dom";
 import Header from "./components/header/Header";
 import Nav from "./components/navigation/Nav";
 import ListContainer from "./components/listContainer/ListContainer";
@@ -7,11 +10,13 @@ import ListContainer from "./components/listContainer/ListContainer";
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header />
-        <Nav />
-        <ListContainer />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Nav />
+          <Route exact path="/" component= { ListContainer }/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
