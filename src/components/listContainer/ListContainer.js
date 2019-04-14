8bac5e1ad8c14cd9d40113c6d43711f2bc7a0641
Rodolfo Perez <rodolfobeste@gmail.com>
2019-04-15 00:56:09 +0300

    Added conditional rendering of modal component

--- src/components/listContainer/ListContainer.js
+++ src/components/listContainer/ListContainer.js
@@ -8,25 +8,26 @@ const SortableContainer = sortableContainer(({children}) => {
   return <ul className="ListContainer">{children}</ul>;
 });
 
-const ListContainer = ({ people, sortList }) => {
+const ListContainer = ({ people, sortList, showModal, toogleModal }) => {
     return(
         
         <React.Fragment>
             {/*/Components inside this container are sortable*/}
-            <SortableContainer onSortEnd={ sortList }>
+            <SortableContainer onSortEnd={ sortList } onClick={ () => toogleModal}>
                 { people ? (
                     people.map((person, index) => (
                         <PersonContainer 
                             key={ person.id } 
                             index={index} 
                             name={person.name} 
-                            company={ person.org_name } />))
+                            company={ person.org_name }
+                            />))
                     ):(
                         ""
                     ) 
                 }
             </SortableContainer>
-            <Modal />
+            <Modal showModal={ showModal } />
         </React.Fragment>
 
     )
--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -2,9 +2,9 @@ import React from "react";
 import "./modal.css";
 import ModalCard from "./modalCard/ModalCard";
 
-const Modal = () => {
+const Modal = ({ showModal }) => {
     return(
-        <div className="Modal">
+        <div className={showModal ? ("Modal isShown") : ("Modal isHidden")}>
             <ModalCard />
         </div>
     )
