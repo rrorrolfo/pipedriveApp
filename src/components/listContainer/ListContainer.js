f5e947ba375852a35afc3a093d93cb196f4e3cad
Rodolfo Perez <rodolfobeste@gmail.com>
2019-04-14 22:50:11 +0300

    Moved sortableElement HOC to a separate file

--- src/components/listContainer/ListContainer.js
+++ src/components/listContainer/ListContainer.js
@@ -1,37 +1,30 @@
 import React from "react";
 import "./listContainer.css";
 import PersonContainer from "./personContainer/PersonContainer";
-import {sortableContainer, sortableElement} from 'react-sortable-hoc';
-import photo from "../../assets/no_pic.jpg";
-
-const SortableItem = sortableElement(({name, company}) => 
-            <li style={{listStyle: "none"}}>
-                <div className="PersonContainer">
-                        <div className="personal_data">
-                            <p>{ name }</p>
-                            <p className="company">{ company }</p>
-                        </div>
-                        <img src={ photo } alt="lady" className="profile_pic"/>
-                    </div>
-            </li>
-        );
+import {sortableContainer} from 'react-sortable-hoc';
 
 const SortableContainer = sortableContainer(({children}) => {
   return <ul >{children}</ul>;
 });
 
 const ListContainer = ({ people, sortList }) => {
-
         return(
+            
+            //Components inside this container are sortable
             <SortableContainer onSortEnd={ sortList }>
-                {people ? (
+                { people ? (
                     people.map((person, index) => (
-                <SortableItem key={ person.id } index={index} name={person.name} company={ person.org_name } />
-                ))
-                ) :(
-                    ""
-                ) }
+                        <PersonContainer 
+                            key={ person.id } 
+                            index={index} 
+                            name={person.name} 
+                            company={ person.org_name } />))
+                    ):(
+                        ""
+                    ) 
+                }
             </SortableContainer>
+
         )
     
 }
--- src/components/listContainer/personContainer/PersonContainer.js
+++ src/components/listContainer/personContainer/PersonContainer.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./personContainer.css";
+import {sortableElement} from 'react-sortable-hoc';
 import photo from "../../../assets/no_pic.jpg";
 
-const PersonContainer = ({ name, company }) => {
-    return(
-
+const PersonContainer = sortableElement(({name, company}) => 
+    <li style={{listStyle: "none"}}>
         <div className="PersonContainer">
             <div className="personal_data">
                 <p>{ name }</p>
@@ -12,8 +12,7 @@ const PersonContainer = ({ name, company }) => {
             </div>
             <img src={ photo } alt="lady" className="profile_pic"/>
         </div>
-
-    )
-}
+    </li>
+    );
 
 export default PersonContainer;
