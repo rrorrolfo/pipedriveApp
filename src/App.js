b1658fae51f71cb9b615f5b5b236833e2c6e7d15
Rodolfo Perez <rodolfobeste@gmail.com>
2019-04-14 22:38:29 +0300

    Returned state to App container

--- src/App.js
+++ src/App.js
@@ -3,11 +3,35 @@ import './styles/App.css';
 import { 
   BrowserRouter,
   Route  } from "react-router-dom";
+import arrayMove from 'array-move';
 import Header from "./components/header/Header";
 import Nav from "./components/navigation/Nav";
 import ListContainer from "./components/listContainer/ListContainer";
 
 class App extends Component {
+
+  state = {
+    people:""
+  }
+
+  componentDidMount() {
+
+        fetch("https://rodolfocompany-860a35.pipedrive.com/v1/persons?api_token=479f2bc15058867bb7dcfdaade60fe25d27c55f4")
+        .then(response => response.json())
+        .then( data => {
+        this.setState({
+        people: data.data
+        }) 
+        console.log(data.data)
+    } )
+
+  }
+
+  onSortEnd = ({oldIndex, newIndex}) => {
+    this.setState(({people}) => ({
+      people: arrayMove(people, oldIndex, newIndex),
+    }));
+  };
 
   render() {
     return (
@@ -15,7 +39,7 @@ class App extends Component {
         <div>
           <Header />
           <Nav />
-          <Route exact path="/" render= { () => <ListContainer /> } />
+          <Route exact path="/" render= { () => <ListContainer people={ this.state.people } sortList={ this.onSortEnd }/> } />
         </div>
       </BrowserRouter>
     );
--- src/components/listContainer/ListContainer.js
+++ src/components/listContainer/ListContainer.js
@@ -1,8 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 import "./listContainer.css";
 import PersonContainer from "./personContainer/PersonContainer";
 import {sortableContainer, sortableElement} from 'react-sortable-hoc';
-import arrayMove from 'array-move';
 import photo from "../../assets/no_pic.jpg";
 
 const SortableItem = sortableElement(({name, company}) => 
@@ -21,47 +20,19 @@ const SortableContainer = sortableContainer(({children}) => {
   return <ul >{children}</ul>;
 });
 
-class ListContainer extends Component {
-
-    state = {
-        people:""
-      }
-    
-      componentDidMount() {
-    
-            fetch("https://rodolfocompany-860a35.pipedrive.com/v1/persons?api_token=479f2bc15058867bb7dcfdaade60fe25d27c55f4")
-            .then(response => response.json())
-            .then( data => {
-            this.setState({
-            people: data.data
-            }) 
-            console.log(data.data)
-        } )
-    
-      }
-    
-      onSortEnd = ({oldIndex, newIndex}) => {
-        this.setState(({people}) => ({
-          people: arrayMove(people, oldIndex, newIndex),
-        }));
-      };
-
-      render() {
-
-        const {people} = this.state;
+const ListContainer = ({ people, sortList }) => {
 
         return(
-            <SortableContainer onSortEnd={this.onSortEnd}>
+            <SortableContainer onSortEnd={ sortList }>
                 {people ? (
                     people.map((person, index) => (
-                <SortableItem key={`person-${index}`} index={index} name={person.name} company={ person.org_name } />
+                <SortableItem key={ person.id } index={index} name={person.name} company={ person.org_name } />
                 ))
                 ) :(
                     ""
                 ) }
             </SortableContainer>
         )
-      }
     
 }
 
