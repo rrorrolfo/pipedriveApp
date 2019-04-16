9039739e21c7d764fcd30076defb785fedb19b8b
Rodolfo Perez <rodolfobeste@gmail.com>
2019-04-16 22:56:41 +0300

    Created Redux Store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
+import { Provider }  from "react-redux";
+import { createStore } from "redux";
+import appReducer from "./reducer/pipedriveApp";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    appReducer
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+    <Provider store={ store }>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
+
 serviceWorker.unregister();
